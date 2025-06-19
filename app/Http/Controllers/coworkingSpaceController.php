dca1007ac610ee87d80fe5863a16e72235f13d77
valerie-alexia <148068674+valerie-alexia@users.noreply.github.com>
2025-06-19 16:20:29 +0700

    typooo
    yes

--- app/Http/Controllers/coworkingSpaceController.php
+++ app/Http/Controllers/coworkingSpaceController.php
@@ -142,7 +142,7 @@ class coworkingSpaceController extends Controller
        }
 
 
-       return view('user.library.coworkingSpace', compact('cwspaces', 'timeSlots', 'schedules', 'date', 'userName', 'userEmail', 'userActiveReservations'));
+       return view('user.library.coWorkingSpace', compact('cwspaces', 'timeSlots', 'schedules', 'date', 'userName', 'userEmail', 'userActiveReservations'));
    }
 
 
