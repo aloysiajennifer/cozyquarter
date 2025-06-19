dad06058ffd0a25806515a7be29e8098fdec8709
c14230207 <c14230207@john.petra.ac.id>
2025-06-19 18:36:05 +0700

    fix:bg color

--- resources/views/admin/book/detail.blade.php
+++ resources/views/admin/book/detail.blade.php
@@ -6,7 +6,7 @@
 
 
 @section('content')
-    <div class="mt-16 p-4 bg-gray-100">
+    <div class="mt-16 p-4">
         <div class="container mx-auto p-4">
             <div class="text-left">
                 <a href="{{ route('book.index') }}">
